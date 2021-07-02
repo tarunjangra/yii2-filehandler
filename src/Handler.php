1ad9e99cd14af48318b1e20dbe2edba629fc7f96
Tarun Jangra <tarun@izap.in>
2021-07-02 02:20:24 -0700

    preserve original implementation

--- src/Handler.php
+++ src/Handler.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace yii\FileSystem;
 
 use Exception;
@@ -10,39 +11,53 @@ use yii\base\BaseObject;
 class Handler extends BaseObject
 {
 
-    private array $matrix = [];
-    private string $driver = 'gd';
+    private array $_matrix = [];
+    private string $_driver = 'gd';
 
-    private AdapterInterface $adapter;
-    private FileProcessor $processor;
+    private AdapterInterface $_adapter;
+    private FileProcessor $_processor;
+    private bool $_preserveOriginalFile = false;
 
     public function init()
     {
         parent::init();
         // validations
-        if(!$this->adapter instanceof AdapterInterface) {
+        if (!$this->_adapter instanceof AdapterInterface) {
             throw new \Exception('Invalid adapter passed.');
         }
-        if(!in_array($this->driver, ['gd','imagick'])) {
+        if (!in_array($this->_driver, ['gd', 'imagick'])) {
             throw new \Exception('Invalid image processing drivers. File handler supports gd and imagick');
         }
-        $this->processor = new FileProcessor($this->matrix, $this->adapter, $this->driver);
+        $this->_processor = new FileProcessor($this->_matrix, $this->_adapter, $this->_driver, $this->_preserveOriginalFile);
     }
 
-    public function setMatrix(array $matrix): void 
+    public function setMatrix(array $matrix): void
     {
-        $this->matrix = $matrix;
+        $this->_matrix = $matrix;
     }
 
+    public function getExtension(): string
+    {
+        return $this->_processor->getExtension();
+    }
+
+    public function getFileName(): string
+    {
+        return $this->_processor->getFileName();
+    }
 
     public function setDriver(string $driver): void
     {
-        $this->driver = $driver;
+        $this->_driver = $driver;
     }
 
     public function setAdapter(AdapterInterface $adapter): void
     {
-        $this->adapter = $adapter;
+        $this->_adapter = $adapter;
     }
 
+    public function setPreserveOriginal(bool $preserve): void
+    {
+        $this->_preserveOriginalFile = $preserve;
+    }
 }
